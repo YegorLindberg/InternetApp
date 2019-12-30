65f9eaaef9421f3f02495cd7314ef9541e899700
YegorLindberg <yegor.lindberg@whitetigersoft.ru>
2019-12-30 12:34:01 +0300

    Added Registration.

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(User user)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                try
+                {
+                    if (dbModel.Users.Any( x => x.fullName == user.fullName))
+                    {
+                        ViewBag.DuplicateMessage = "User already exist.";
+                        return View("AddOrEdit", user);
+                    }
+
+                    dbModel.Users.Add(user);
+                    dbModel.SaveChanges();
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    foreach (DbEntityValidationResult validationError in ex.EntityValidationErrors)
+                    {
+                        Response.Write("Object: " + validationError.Entry.Entity.ToString());
+                        Response.Write("\n");
+                        foreach (DbValidationError err in validationError.ValidationErrors)
+                        {
+                            Response.Write(err.ErrorMessage + "\n");
+                        }
+                    }
+                }
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successfull.";
+            return View("AddOrEdit");
+        }
+    }
+}
---
+++ WebApplication1/DataBase/dbo.User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [roleId] INT NULL DEFAULT 0, 
+    [email] NCHAR(50) NOT NULL, 
+    [password] NCHAR(20) NOT NULL, 
+    [fullName] NCHAR(30) NOT NULL, 
+    [description] NCHAR(100) NULL
+)
